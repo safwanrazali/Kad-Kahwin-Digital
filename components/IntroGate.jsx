eaaf9232264f67a89b9ea2d411d2e48955be000f
safwanrazali <a.safwanrazali@gmail.com>
2026-07-14 16:13:16 +0800

    feat: update IntroGate styles and implement scroll locking functionality

--- components/IntroGate.jsx
+++ components/IntroGate.jsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function IntroGate({ brideName, groomName }) {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    // Kunci scroll body selagi kad belum dibuka
+    if (!open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [open]);
 
   const handleOpen = () => {
     window.dispatchEvent(new Event("start-music"));
@@ -17,7 +29,6 @@ export default function IntroGate({ brideName, groomName }) {
       <div className="intro-gate-inner">
         <div className="intro-gate-names">
           {brideName} <br /> &amp; <br /> {groomName}
-
         </div>
         <button type="button" className="btn-romantic intro-gate-btn" onClick={handleOpen}>
           Buka
